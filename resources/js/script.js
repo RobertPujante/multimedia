0d28c4c9680ab78bba0c67974273f027c0a6d482
RobertPujante <moralesso152842@gmail.com>
2020-12-12 01:58:33 +0800

    neumorphism effects

---
+++ resources/js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(() => {
+    var audio = $('#audio')[0];
+    var count = (-1);
+    
+    if (audio.ended) {
+        $('#artist').removeClass('rotation');
+    }
+
+    $('#btn-repeat').on('click', function(){
+        audio.loop = true;
+    });
+
+    $('#btn-stop').on('click', function(){
+        $('#btn-play').show();
+        $('#btn-pause').hide();
+        audio.load();
+        if ($('#artist').hasClass('rotation')) {
+            $('#artist').removeClass('rotation');
+        }
+    });
+
+    $('#btn-play').on('click', function(){
+        audio.play();
+        $(this).hide();
+        $('#btn-pause').show();
+        $('#btn-pause').addClass('in-active');
+        $('#artist').addClass('rotation');
+    });
+
+    $('#btn-pause').on('click', function(){
+        audio.pause();
+        $(this).hide();
+        $('#btn-play').show();
+        $('#artist').removeClass('rotation');
+    });
+
+    $('#btn-volume').on('click', function(){       
+        count = count + 1;
+
+        if (count == 0) {
+            audio.volume = 0;
+            $('.volume-x').removeClass('d-none');
+            $('.volume').addClass('d-none');
+            $('.volume-1').addClass('d-none');
+            $('.volume-2').addClass('d-none');
+            $(this).attr('title', 'Volume 0%');
+        }else if(count == 1){
+            audio.volume = 0.25;
+            $('.volume-x').addClass('d-none');
+            $('.volume').removeClass('d-none');
+            $('.volume-1').addClass('d-none');
+            $('.volume-2').addClass('d-none');
+            $(this).attr('title', 'Volume 25%');
+        }else if(count == 2){
+            audio.volume = 0.5;
+            $('.volume-x').addClass('d-none');
+            $('.volume').addClass('d-none');
+            $('.volume-1').removeClass('d-none');
+            $('.volume-2').addClass('d-none');
+            $(this).attr('title', 'Volume 50%');
+        }else if(count == 3){
+            audio.volume = 1;
+            $('.volume-x').addClass('d-none');
+            $('.volume').addClass('d-none');
+            $('.volume-1').addClass('d-none');
+            $('.volume-2').removeClass('d-none');
+            $(this).attr('title', 'Volume 100%');
+            count = (-1);
+        }
+    });
+});
