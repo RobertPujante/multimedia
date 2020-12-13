dabb6ee735eb029ed13c0358201f4e57d76015b4
RobertPujante <moralesso152842@gmail.com>
2020-12-13 18:06:22 +0800

    added new functionalities

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,6 +9,20 @@ $(document).ready(() => {
         $('#song-title').html(res[0].title);
         $('#artist-info').html(res[0].artist);
 
+        $('#btn-menu').on('click', function(){
+            let x = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
+            let m = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>';
+            let currentFeather = $(this).children()[0].classList[1];
+            $(this).toggleClass('in-active');
+            if(currentFeather == 'feather-menu'){
+                $(this).empty();
+                $(this).prepend(x);
+            }else{
+                $(this).empty();
+                $(this).prepend(m);
+            }
+        });
+
         $('#btn-settings').on('click', function(){
             $(this).toggleClass('in-active');
             $('#dropdown-settings').toggle(100);
@@ -37,7 +51,6 @@ $(document).ready(() => {
             audio.play();
             $(this).hide();
             $('#btn-pause').show();
-            $('#btn-pause').addClass('in-active');
             $('#artist').addClass('rotation');
         });
     
@@ -50,28 +63,46 @@ $(document).ready(() => {
         
         $('#btn-back').on('click', function(){
             list = list - 1;
-            audio.load();
-            $('#artist').removeClass('rotation');
-            $('#btn-play').show();
-            $('#btn-pause').hide();
+            if(list > (-1)){
+                audio.load();
+                $('#artist').removeClass('rotation');
+                $('#btn-play').show();
+                $('#btn-pause').hide();
 
-            $('#audio').attr('src', 'resources/audios/' + res[list].audio);
-            $('#artist').attr('src', 'resources/images/' + res[list].image);
-            $('#song-title').html(res[list].title);
-            $('#artist-info').html(res[list].artist);
+                $('#audio').attr('src', 'resources/audios/' + res[list].audio);
+                $('#artist').attr('src', 'resources/images/' + res[list].image);
+                $('#song-title').html(res[list].title);
+                $('#artist-info').html(res[list].artist);
+                if($('#btn-next').hasClass('in-active')){
+                    $('#btn-next').removeClass('in-active');
+                    $('#btn-next').attr('disabled', false);
+                }
+            } else {
+                $(this).addClass('in-active');
+                $(this).attr('disabled', true);
+            }
         });
 
         $('#btn-next').on('click', function(){
             list = list + 1;
-            audio.load();
-            $('#artist').removeClass('rotation');
-            $('#btn-play').show();
-            $('#btn-pause').hide();
+            if(res.length > list){
+                audio.load();
+                $('#artist').removeClass('rotation');
+                $('#btn-play').show();
+                $('#btn-pause').hide();
 
-            $('#audio').attr('src', 'resources/audios/' + res[list].audio);
-            $('#artist').attr('src', 'resources/images/' + res[list].image);
-            $('#song-title').html(res[list].title);
-            $('#artist-info').html(res[list].artist);
+                $('#audio').attr('src', 'resources/audios/' + res[list].audio);
+                $('#artist').attr('src', 'resources/images/' + res[list].image);
+                $('#song-title').html(res[list].title);
+                $('#artist-info').html(res[list].artist);
+                if($('#btn-back').hasClass('in-active')){
+                    $('#btn-back').removeClass('in-active');
+                    $('#btn-back').attr('disabled', false);
+                }
+            } else {
+                $(this).addClass('in-active');
+                $(this).attr('disabled', true);
+            }
         });
     
         $('#btn-volume').on('click', function(){       
