1b88e13c0783191a417c212052c47812d0d186b1
RobertPujante <moralesso152842@gmail.com>
2021-01-04 09:51:38 +0800

    fixed layout for audio tracker

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,19 @@
 feather.replace();
 
 $(function(){
+    "use strict";
+
     let audio = $('#audio')[0];
-    audio.preload = "auto";
     let list = 0;
 
+    audio.preload = "auto";
+
     $.get('resources/js/data.json', function(res){
-        showTable();
 
         $('#audio').attr('src', 'resources/audios/' + res[0].audio);
-        $('#artist').attr('src', 'resources/images/' + res[0].image);
-        $('#artist').attr('alt', res[0].title);
+
         $('#song-title').html(res[0].title);
-        $('#artist-info').html(res[0].artist);        
+        $('#artist-info').html(res[0].artist);
         
         let isClicked = 0;
         $('#btn-shuffle').on('click', function(){           
@@ -48,7 +49,6 @@ $(function(){
             audio.play();
             
             $('#btn-pause').show();
-            $('#artist').addClass('rotation');
         });
     
         $('#btn-pause').on('click', function(){
@@ -59,16 +59,13 @@ $(function(){
             audio.pause();
 
             $('#btn-play').show();
-            $('#artist').removeClass('rotation');
         });
         
         $('#btn-back').on('click', function(){
             list = list - 1;
-            $('#artist').removeClass('rotation');
 
             if(list > (-1)){
                 $('#audio').attr('src', 'resources/audios/' + res[list].audio);
-                $('#artist').attr('src', 'resources/images/' + res[list].image);
                 $('#song-title').html(res[list].title);
                 $('#artist-info').html(res[list].artist);
                 if($('#btn-next').hasClass('in-active')){
@@ -79,19 +76,16 @@ $(function(){
                 $('#btn-play').trigger('click');
             } else {
                 list = res.length;
-                // $(this).addClass('in-active');
-                // $(this).attr('disabled', true);
-                $('#artist').addClass('rotation');
             }
+
+            showTable(list);
         });
 
         $('#btn-next').on('click', function(){
             list = list + 1;
-            $('#artist').removeClass('rotation');
 
             if(res.length > list){
                 $('#audio').attr('src', 'resources/audios/' + res[list].audio);
-                $('#artist').attr('src', 'resources/images/' + res[list].image);
                 $('#song-title').html(res[list].title);
                 $('#artist-info').html(res[list].artist);
                 if($('#btn-back').hasClass('in-active')){
@@ -102,15 +96,13 @@ $(function(){
                 $('#btn-play').trigger('click');
             } else {
                 list = 0;
-                // $(this).addClass('in-active');
-                // $(this).attr('disabled', true);
-                $('#artist').addClass('rotation');
             }
+
+            showTable(list);
         });
         
         $('#btn-volume').on('click', function(){
             $('.range').toggleClass('d-none');
-            // $(this).toggleClass('in-active');
         });
 
         $('.range').on('mouseleave', function(){
@@ -151,55 +143,57 @@ $(function(){
                 $('.volume-1').addClass('d-none');
                 $('.volume-2').removeClass('d-none');
             }
-        });
+        });        
 
-        // $('#audio-track').on('change', function(){
-        //     let totalDuration = audio.duration * 1000;
-        //     $(this).val(totalDuration);
-        // });
-
-        $('#btn-show').on('click', function(){
+        $('#btn-show').on('click', function(){          
             $(this).toggleClass('rotate');
-            $('#song-list-table').toggleClass('d-none');
             $('.container-wrapper').toggleClass('justify-content-between');
-            $('.artist-wrapper').toggleClass('d-flex flex-row justify-content-start w-100');
-            $('.artist-image').toggleClass('shrink-image');
-            $('.artist-details').toggleClass('text-left ml-4');
+            $('#song-list-table').toggleClass('d-none');
+            $('.music-icon').toggleClass('shrink-music-icon');
             $('#artist-info').toggleClass('shrink-size-p');
             $('#song-title').toggleClass('shrink-size-h2');
         });
-    
-        audio.onended = function(){
-            $('#btn-next').trigger('click');
-            $('#artist').addClass('rotation');
-            $('#btn-play').hide();
-            $('#btn-pause').show();
-        }
 
-        function showTable(){
+        $('.table-row').on('click', function(){
+            let id = $(this).attr('id');
+            $('#audio').attr('src', )
+        });
+
+        function showTable(list = ''){
             let songs = [];
-            res.forEach(element => {
-                let play_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-play"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>';
-                let content = '\
-                <tr>\
-                    <td>\
-                        <div class="d-flex flex-column align-items-start">\
-                            <a href="now-playing.php?id='+element.id+'" class="btn-play text-dark">\
-                                '+play_icon+'\
-                            </a>\
-                        </div>\
-                    </td>\
-                    <td>\
-                        <span>'+element.title+'</span>\
-                        <span>'+element.artist+'</span>\
-                    </td>\
-                    <td>3:12</td>\
-                </tr>';
+
+            $('#tbody').empty();
+            for (let i = 0; i < res.length; i++) {
+                let content = '';
+                let el = res[i];
+                if (list == i) {
+                    content += '\
+                    <tr class="table-primary table-row" id="'+el.id+'">\
+                        <td>\
+                            <span>'+el.title+'</span>\
+                            <span> - </span>\
+                            <span>'+el.artist+'</span>\
+                        </td>\
+                        <td>3:12</td>\
+                    </tr>';
+                }else{
+                    content += '\
+                    <tr class="table-row" id="'+el.id+'">\
+                        <td>\
+                            <span>'+el.title+'</span>\
+                            <span> - </span>\
+                            <span>'+el.artist+'</span>\
+                        </td>\
+                        <td>3:12</td>\
+                    </tr>';
+                }
                 songs.push(content);
-            });
+            }
 
             $('#tbody').append(songs);
         }
+
+        showTable();
 
         function audioTracker(){
             let minsDuration = Math.floor(audio.duration / 60);
@@ -217,6 +211,12 @@ $(function(){
                 $('#current-time').html(mins + ":" + secs);
     
             }, 0);
+        }
+
+        audio.onended = function(){
+            $('#btn-next').trigger('click');
+            $('#btn-play').hide();
+            $('#btn-pause').show();
         }
     });
 
